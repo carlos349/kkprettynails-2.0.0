d6dfd8d267b58ea164fd8d14d24134977384aa60
CristiansSans <cristiansleonardo@gmail.com>
2020-03-12 02:21:47 -0400

    all kind of shit

--- src/views/Agendamiento.vue
+++ src/views/Agendamiento.vue
@@ -1,14 +1,14 @@
 <template>
     <div>
         <base-header class="header pb-8 pt-5 pt-lg-8 d-flex align-items-center"
-            style="min-height: 50px; background-image: url(img/theme/clients.jpg); background-size: cover; background-position: center top;height:45vh;">
+            style="min-height: 50px; background-image: url(img/theme/agenda.jpg); background-size: cover; background-position: center top;height:45vh;">
             <!-- Mask -->
             <span style="background-color:#172b4d !important" class="mask  opacity-7"></span>
             <!-- Header container -->
             <div class="container-fluid d-flex align-items-center">
                 <div class="row">
                     <div class="col-lg-12 col-md-12">
-                        <h1 class="display-2 text-white w-100">Sección de Agendamiento</h1>
+                        <h1 class="display-2 text-white w-100">Sección de Agenda</h1>
                         <p class="text-white mt-0 mb-5">Esta es la sección administrativa de agendamiento, aquí podrás registrar, editar y visualizar tu agenda.</p>
                         <a @click="modals.modal1 = true"  class="btn btn-success text-white">Agendar</a>
                         <a @click="modals.modal2 = true"  class="btn btn-info text-white">Ventas por procesar</a>
@@ -20,24 +20,32 @@
         <modal :show.sync="modals.modal1"
                body-classes="p-0"
                modal-classes="modal-dialog-centered modal-xl">
-            <card type="secondary" shadow
-                  header-classes="bg-white pb-5"
-                  body-classes="px-lg-5 py-lg-5"
-                  class="border-0">
-                <form-wizard color="#214d88" back-button-text="Regresar" next-button-text="Siguiente" finish-button-text="¡Agendar!">
-                    <h2 slot="title">Datos de agendamiento</h2>
-                    <tab-content icon="ni ni-bullet-list-67" title="Servicios">
-                        <div class="row">
-                            <base-button v-on:click="countServices[index].count++" v-for="(name, index) in services" :key="name" class="col-5 mb-2 mx-auto" type="default">
-                                <badge class="float-left text-white" pill type="default"><i class="ni ni-single-02"></i>{{name.prestadores.length}}</badge>
-                                <span class="float-left">{{name.nombre}}</span>
-                                <badge class="text-default float-right" type="white">{{countServices[index].count}}</badge>
-                                <badge class="text-default float-right" type="danger"><i class="ni ni-fat-delete"></i></badge>
-                                
-                            </base-button>
+            <card type="secondary" shadow header-classes="bg-white pb-5" body-classes="px-lg-5 py-lg-5" class="border-0">
+
+                  <!-- WIZARD -->
+
+                <form-wizard color="#214d88" error-color="#f5365c" back-button-text="Regresar" next-button-text="Siguiente" finish-button-text="¡Agendar!">
+
+                    <h2 v-if="registerDate.valid == true" slot="title">Datos de agendamiento</h2>
+                    <h2 v-else slot="title" class="text-danger">¡Debe completar los datos!</h2>
+
+                    <tab-content icon="ni ni-bullet-list-67" title="Servicios" :before-change="validateWizardOne">
+                        <!-- <base-button size="sm" v-on:click="initialState()" type="secondary" class="text-default p-0 float-right"><i class="ni ni-fat-delete"></i> -->
+                        </base-button>
+                        <div class="row ml-3">
+                            <div v-for="(name, index) in services" :key="name" class="col-6 mb-2 mx-auto">
+                                <base-button v-on:click="pushService(name.prestadores,name.nombre,name.tiempo, name.comision, name.precio,name.descuento,index)" class="col-10"  type="default">
+                                    <badge class="float-left text-white" pill type="default">
+                                        <i class="ni ni-single-02"></i>{{name.prestadores.length}}
+                                    </badge>
+                                    <span class="float-left">{{name.nombre}}</span>
+                                    <badge class="text-default float-right" type="white">{{countServices[index].count}}</badge>
+                                </base-button>
+                            </div>
                         </div>
                     </tab-content>
-                    <tab-content icon="ni ni-collection" title="Información">
+
+                    <tab-content icon="ni ni-collection" title="Información" :before-change="validateWizardTwo">
                         <div class="row">
                             <vue-single-select
                                 v-model="registerDate.client"
@@ -60,15 +68,20 @@
                             
                         </div>  
                     </tab-content>
-                    <tab-content icon="ni ni-watch-time" title="Disponibilidad">
+
+                    <tab-content icon="ni ni-watch-time" title="Disponibilidad" :before-change="validateWizardThree">
                         
                     </tab-content>
                     <tab-content icon="ni ni-check-bold" title="Finalizar">
                         Yuhuuu! This seems pretty damn simple
                     </tab-content>
                 </form-wizard>
+
+                <!-- END WIZARD -->
+
             </card>
         </modal>
+        <vue-cal style="height: 250px" />
         
     </div>
 </template>
@@ -77,9 +90,6 @@
     import VueCal from 'vue-cal'
   import 'vue-cal/dist/vuecal.css'
   import 'vue-cal/dist/i18n/es.js'
-  import 'vue-cal/dist/vuecal.common.js'
-  import 'vue-cal/dist/vuecal.umd.js'
-  import 'vue-cal/dist/vuecal.umd.min.js'
   import VueBootstrap4Table from 'vue-bootstrap4-table'
   import flatPicker from "vue-flatpickr-component";
   import "flatpickr/dist/flatpickr.css";
@@ -124,8 +134,12 @@
          countServices:[],
          registerDate: {
              services:[],
+             servicesShow:[],
+             employe:[],
              client:null,
-             date:null
+             block:null,
+             date:null,
+             valid:true,
          },
          services:[],
          locale: 'es',
@@ -261,6 +275,77 @@
                 }
             })
         },
+        pushService(prestadores,nombre,tiempo, comision, precio,descuento,count){
+            if (this.registerDate.services == '') {
+                this.registerDate.services.push({'servicio': nombre, 'comision': comision, 'precio': precio, 'descuento': descuento})
+                this.registerDate.servicesShow.push(nombre)
+                for (let index = 0; index < prestadores.length; index++) {
+                    this.registerDate.employe.push(prestadores[index])
+                }
+            }
+            else{
+                var inspector = true
+                var inspector2 = false
+                for (let index = 0; index < this.registerDate.services.length; index++) {
+                    if (this.registerDate.services[index].servicio == nombre || this.registerDate.services[index].servicio == nombre + "("+ this.countServices[count].count +")" ) {
+                    this.registerDate.servicesShow.splice(index,1)
+                    this.registerDate.servicesShow.push(nombre+ "(" + (parseFloat(this.countServices[count].count) +1) + ")")
+                    inspector = false
+                    
+                    break  
+                    }
+                }
+                if (inspector == true) {
+                    this.registerDate.services.push({'servicio': nombre, 'comision': comision, 'precio': precio, 'descuento': descuento})
+                    this.registerDate.servicesShow.push(nombre)
+                }
+                for (let c = 0; c < prestadores.length; c++) {
+                    for (let index = 0; index < this.registerDate.employe.length; index++) {
+                        if (prestadores[c] == this.registerDate.employe[index]) {
+                            inspector2 = true
+                            break
+                        }
+                        
+                    }  
+                    if (inspector2 == false) {
+                        this.registerDate.employe.push(prestadores[c])
+                    }
+                }   
+            }
+            this.duracion = parseFloat(this.duracion) + parseFloat(tiempo)
+            this.countServices[count].count++  
+            console.log(this.registerDate)  
+        },
+        validateWizardOne(){
+            if (this.registerDate.services != '') { 
+                this.registerDate.valid = true
+                return true
+            }
+            else{
+                this.registerDate.valid = false 
+                return false
+            } 
+        },
+        validateWizardTwo(){
+               if (this.registerDate.services != '') { 
+                   this.registerDate.valid = true
+                   return true
+               }
+               else{
+                   this.registerDate.valid = false 
+                   return false
+               } 
+        },
+        validateWizardThree(){
+               if (this.registerDate.services != '') { 
+                   this.registerDate.valid = true
+                   return true
+               }
+               else{
+                   this.registerDate.valid = false 
+                   return false
+               } 
+        }
     },
     mounted() {
       
