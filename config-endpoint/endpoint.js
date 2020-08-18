69370f3f3992a1e2f428c5c875cd0d84c9e38bf7
carlos349 <carlos.gomes349@gmail.com>
2020-08-18 16:33:02 -0400

    add build front

--- config-endpoint/endpoint.js
+++ config-endpoint/endpoint.js
@@ -1,4 +1,4 @@
-const server = 'localhost'
+const server = '18.231.122.2'
 const portClient = '3389'
 const endpointTarget = `http://${server}:4200`
 const imgEndpoint = `${endpointTarget}/static/users/`
