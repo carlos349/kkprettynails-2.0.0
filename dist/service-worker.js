c10cbc8f775f88176dc5c91101fe4cf94cecc695
carlos349 <carlos.gomes349@gmail.com>
2020-10-06 01:20:25 -0400

    build error pay methods

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.c3ecc68a4b278d83e589f800559f625d.js"
+  "/precache-manifest.aaa42fe93e40919e70d463fd18ad359c.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-argon-dashboard"});
