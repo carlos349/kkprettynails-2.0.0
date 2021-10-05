2484386cab42504e0b58c7826a5275009824f342
carlos capacho <carlos.gomes349@gmail.com>
2021-10-05 01:31:35 -0300

    fixed endpoint

--- config-endpoint/endpoint.js
+++ config-endpoint/endpoint.js
@@ -1,5 +1,5 @@
 const serverL = 'localhost:3200'
-const server = 'kkprettynailsback.syswa.net'
+const server = '52.67.12.97:4200'
 const serverQa = '18.231.122.2:3200'
 const portClient = '3389'
 const endpointTarget = `http://${serverL}`
--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.f2a1b68abbcd4f24951466755b86cabe.js"
+  "/precache-manifest.4054fb78dbdf743fc665730bcde01a1f.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-argon-dashboard"});
