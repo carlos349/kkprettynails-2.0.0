63e8e1e68abc760d4427dbd29d32a409be443e44
Cristians Sanchez <cristiansleonardo@gmail.com>
2022-07-08 22:46:35 -0400

    console fix

--- src/views/Clients.vue
+++ src/views/Clients.vue
@@ -402,7 +402,6 @@
 import axios from 'axios'
 import endPoint from '../../config-endpoint/endpoint.js'
 import VueBootstrap4Table from 'vue-bootstrap4-table'
-import vueCustomScrollbar from 'vue-custom-scrollbar'
 import EventBus from '../components/EventBus'
 import jwtDecode from 'jwt-decode'
 import router from '../router'
