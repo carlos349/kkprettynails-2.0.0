af48b44004909596e7d1e27b55c4964e401e9ec4
carlos capacho <carlos.gomes349@gmail.com>
2022-10-14 14:35:23 -0300

    add victoria to angela logic

--- dist/service-worker.js
+++ dist/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.735779c163536e6706f97de457bece6c.js"
+  "/precache-manifest.b3cfb531a33e9d9d44433559e8edbf04.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-argon-dashboard"});
